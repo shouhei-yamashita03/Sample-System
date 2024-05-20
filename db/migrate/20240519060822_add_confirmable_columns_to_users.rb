814209c6319819a0a7a5b0f4a824a7cc522485c4
shouhei-yamashita03 <xiangpingshanxia757@gmail.com>
2024-05-21 01:29:03 +0900

    feat/devise_1完了

---
+++ db/migrate/20240519060822_add_confirmable_columns_to_users.rb
@@ -0,0 +1,6 @@
+class AddConfirmableColumnsToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmation_sent_at, :datetime
+  end
+end
